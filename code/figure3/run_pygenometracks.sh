fce73f00b293fdcd0b39e3add0020b71f0ad26d3
taiwaness <arbula@gmail.com>
2026-03-16 05:00:59 +0100

    ntu scripts

--- code/figure3/run_pygenometracks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pyGenomeTracks \
-    --tracks ./gene_tracks.ini \
-    --region chr5:122061195-122080259 \ #LOX: chr5:122061195-122080259; THY1: chr11:119410000-119425000
-    --outFileName ./gene_plot.pdf \
-    --fontSize 10 \
-    --dpi 300 \
-    --width 40
